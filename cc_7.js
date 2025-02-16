3be3aad9bdd5a4c881cccac5991edda266716376
Ashley Le <ashleyle@usf.edu>
2025-02-16 16:48:57 -0500

    added toFixed in Task 1 to display the cents in total

--- cc_7.js
+++ cc_7.js
@@ -1,11 +1,11 @@
 //Task 1: Function Declaration
 function calculateInvoice(subtotal, taxRate, discount) {
     let invoice = (subtotal + (subtotal * taxRate)) - discount; //formula to find the invoice
-    return `Total Invoice: $${invoice}`; //displaying the total
+    return `Total Invoice: $${invoice.toFixed(2)}`; //displaying the total
 }; 
 
-console.log(calculateInvoice(100, 0.08, 5)); //"Total Invoice: $103"
-console.log(calculateInvoice(500, 0.1, 20));//"Total Invoice: $530"
+console.log(calculateInvoice(100, 0.08, 5)); //"Total Invoice: $103.00"
+console.log(calculateInvoice(500, 0.1, 20));//"Total Invoice: $530.00"
 
 //Task 2: Function Expression
 function calculateHourlyWage(salary, hoursPerWeek) {
