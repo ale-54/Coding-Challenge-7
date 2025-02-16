3b109ce83852ca8e2b2fe4a853676f45c212a91b
Ashley Le <ashleyle@usf.edu>
2025-02-16 18:49:54 -0500

    Task 5 - Business Loan Interest Calculation

--- cc_7.js
+++ cc_7.js
@@ -48,3 +48,12 @@ const calculateShippingCost = (weight, location, expedited = false) => {
 
 console.log(calculateShippingCost(10, "USA", true)); //Shipping Cost: $20.00"
 console.log(calculateShippingCost(5, "Canada", false)); //"Shipping Cost: $13.50"
+
+//Task 5: Returning Values
+function calculateLoanInterest(principal, rate, years) {
+    let loanInterest = principal * rate * years; //formula to find the interest
+    return `Total Interest: $${loanInterest.toFixed(2)}`; //displaying total interest
+}; 
+
+console.log(calculateLoanInterest(1000, 0.05, 3)); //"Total Interest: $150.00"
+console.log(calculateLoanInterest(5000, 0.07, 5)); //"Total Interest: $1750.00"
